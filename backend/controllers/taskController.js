5073f30f3a2a5a8e3c69ab7eb6e3f3b0d199209a
Deepika Somasundaram <sdeepikas88@gmail.com>
2024-03-13 20:18:39 -0400

    Patch and delete methods

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -26,12 +26,43 @@ const getTasks=async(req,res)=>{
 const getSingleTask=async(req,res)=>{
     const {_id}=req.params;
     if(!mongoose.Types.ObjectId.isValid(_id))
-        return res.status(404).json({message:"Task not found"});
+        return res.status(404).json({error:"Task not found"});
     try{
         const singleTask=await taskModel.findById({_id});
         res.status(200).json(singleTask);
     }catch(e){
         res.status(400).json({error:e.message});
     }
-}
-module.exports={createTask,getTasks,getSingleTask};
+};
+
+//PATCH method
+
+const updateTask=async(req,res)=>{
+    const {id}=req.params;
+    if(!mongoose.Types.ObjectId.isValid(id))
+        return res.status(404).json({error:"Task not found"});
+    try{
+        const taskToUpdate=await taskModel.findByIdAndUpdate({
+            _id:id,
+        },{...req.body});
+        res.status(200).json(taskToUpdate);
+    }catch(e){
+        res.status(400).json({error:e.message})
+    }
+};
+
+
+//Delete method
+
+const deleteTask=async(req,res)=>{
+    const {id}=req.params;
+    if(!mongoose.Types.ObjectId.isValid(id))
+        return res.status(404).json({error:"Task not found"});
+    try{
+        const tasktoDelete=await taskModel.findByIdAndDelete({_id:id});
+        res.status(200).json(tasktoDelete);
+    }catch(e){
+        res.status(400).json({error:e.message});
+    }
+};
+module.exports={createTask,getTasks,getSingleTask,updateTask,deleteTask};
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,9 +1,11 @@
 const express=require('express');
-const { createTask, getTasks, getSingleTask } = require('../controllers/taskController');
+const { createTask, getTasks, getSingleTask, updateTask, deleteTask } = require('../controllers/taskController');
 const router=express.Router();
 
 router.post('/',createTask);
 router.get('/',getTasks);
 router.get('/:_id',getSingleTask);
+router.patch('/:id',updateTask);
+router.delete('/:id',deleteTask);
 
 module.exports=router;
